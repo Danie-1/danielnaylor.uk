a5c325a7ba56335ce8705848ce3e0996f9fc7dfd
Daniel Naylor <daniel@danielnaylor.uk>
2026-05-10 15:42:14 +0100

    fix: set env vars in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 # export $(cat .env | xargs)
 # uv run gunicorn app:app -b :$PORT -w 3
+export BASE_FOLDER=/home/daniel/Documents/latex/notes/
+export PORT=8000
 docker compose build
 docker compose up
